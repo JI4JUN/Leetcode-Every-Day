668a6ad67ccd3cf2dfcd00903a23b3fb8389aa95
qiujiajun <545509146@qq.com>
2024-06-17 20:08:57 +0800

    chore: init

---
+++ Unknown/[2798]满足目标工作时长的员工数目/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=2798 lang=typescript
+ *
+ * [2798] 满足目标工作时长的员工数目
+ */
+
+// @lc code=start
+function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
+
+};
+// @lc code=end
+
