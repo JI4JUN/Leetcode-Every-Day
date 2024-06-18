524f03a46bbd8a618f57da64e5fd02045df48e54
qiujiajun <545509146@qq.com>
2024-06-18 23:35:43 +0800

    feat: solution of 78

---
+++ leetcode/array/[78]子集/index.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=typescript
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+function subsets(nums: number[]): number[][] {
+    const subsets: number[][] = [];
+
+    const backtrack = (start: number, path: number[]): void => {
+        subsets.push([...path]);
+
+        for (let i = start; i < nums.length; ++i) {
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    backtrack(0, []);
+
+    return subsets;
+};
+// @lc code=end
+
