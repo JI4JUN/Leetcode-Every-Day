07107a65f27384e32e5800b973b9afcad97f041d
qiujiajun <545509146@qq.com>
2024-06-19 20:00:33 +0800

    feat: solution of 90

---
+++ leetcode/backtracking/[90]子集Ii/index.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=90 lang=typescript
+ *
+ * [90] 子集 II
+ */
+
+// @lc code=start
+function subsetsWithDup(nums: number[]): number[][] {
+    const subsets: number[][] = [];
+
+    const backtrack = (start: number, path: number[]): void => {
+        subsets.push([...path]);
+
+        for (let i = start; i < nums.length; ++i) {
+            if (i > start && nums[i] === nums[i - 1]) {
+                continue;
+            }
+
+            path.push(nums[i]);
+            backtrack(i + 1, path);
+            path.pop();
+        }
+    }
+
+    nums.sort();
+    backtrack(0, []);
+
+    return subsets;
+
+};
+// @lc code=end
+
