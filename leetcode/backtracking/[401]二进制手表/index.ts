145cf5c8a634f042720173e65b61d0f2f75a63bc
qiujiajun <545509146@qq.com>
2024-06-19 14:38:45 +0800

    feat: solution of 401

---
+++ leetcode/backtracking/[401]二进制手表/index.ts
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode.cn id=401 lang=typescript
+ *
+ * [401] 二进制手表
+ */
+
+// @lc code=start
+function timeFormatter(hour: number, minute: number): string {
+    return `${hour}:${minute < 10 ? '0' + minute : minute}`
+}
+
+function readBinaryWatch(turnedOn: number): string[] {
+    if (turnedOn > 8) {
+        return [];
+    }
+
+    const hours: number[] = [1, 2, 4, 8];
+    const minutes: number[] = [1, 2, 4, 8, 16, 32];
+    const result: string[] = [];
+
+    const backtrack = (hour: number, minute: number, turnedOn: number, hours: number[], minutes: number[], isCalcHour: boolean): void => {
+        if (turnedOn === 0) {
+            result.push(timeFormatter(hour, minute));
+
+            return;
+        }
+
+        if (isCalcHour) {
+            for (const i in hours) {
+                const tmpHour = hours[i];
+                if ((hour + tmpHour) < 12) {
+                    const tmpHours = hours.slice(+i + 1);
+                    backtrack(hour + tmpHour, minute, turnedOn - 1, tmpHours, minutes, true);
+                }
+            }
+        }
+
+        for (const i in minutes) {
+            const tmpMinute = minutes[i];
+            if ((minute + tmpMinute) < 60) {
+                const tmpMintues = minutes.slice(+i + 1);
+                backtrack(hour, minute + tmpMinute, turnedOn - 1, hours, tmpMintues, false);
+            }
+        }
+    };
+
+    backtrack(0, 0, turnedOn, hours, minutes, true);
+
+    return result;
+}
+// @lc code=end
+
