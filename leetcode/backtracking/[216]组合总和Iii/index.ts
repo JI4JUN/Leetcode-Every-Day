08441ba996ed1ee40d3bbf9978dff24e8ae3310f
qiujiajun <545509146@qq.com>
2024-06-24 23:47:12 +0800

    feat: solution of 216

---
+++ leetcode/backtracking/[216]组合总和Iii/index.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=216 lang=typescript
+ *
+ * [216] 组合总和 III
+ */
+
+// @lc code=start
+function combinationSum3(k: number, n: number): number[][] {
+    const result: number[][] = [];
+    const path: number[] = [];
+
+    if (n < k) {
+        return result;
+    }
+
+    const backtrack = (n: number, k: number, index: number): void => {
+        if (path.length === k && n === 0) {
+            result.push([...path]);
+
+            return;
+        }
+
+        for (let i = index; i <= 9; i++) {
+            if (n - i < 0) {
+                return;
+            }
+
+            path.push(i);
+            backtrack(n - i, k, i + 1);
+            path.pop();
+        }
+    }
+
+    backtrack(n, k, 1);
+
+    return result;
+};
+// @lc code=end
+
