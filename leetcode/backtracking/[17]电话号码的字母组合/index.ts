51a4b4ed86a2aaaea4023d86a4fb710d77247bb6
qiujiajun <545509146@qq.com>
2024-06-25 12:04:58 +0800

    feat: solution of 17

---
+++ leetcode/backtracking/[17]电话号码的字母组合/index.ts
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode.cn id=17 lang=typescript
+ *
+ * [17] Letter Combinations of a Phone Number
+ */
+
+// @lc code=start
+function letterCombinations(digits: string): string[] {
+    const result: string[] = [];
+
+    if (['', '0', '1', '*', '#'].includes(digits)) {
+        return result;
+    }
+
+    const letterMap: { [digits: string]: string[] } = {
+        1: [],
+        2: ['a', 'b', 'c'],
+        3: ['d', 'e', 'f'],
+        4: ['g', 'h', 'i'],
+        5: ['j', 'k', 'l'],
+        6: ['m', 'n', 'o'],
+        7: ['p', 'q', 'r', 's'],
+        8: ['t', 'u', 'v'],
+        9: ['w', 'x', 'y', 'z'],
+        0: [],
+        '*': [],
+        '#': [],
+    }
+
+    const path: string[] = [];
+
+    const backtrack = (path: string[], currentIdx: number): void => {
+        if (currentIdx === digits.length) {
+            result.push(path.join(''));
+
+            return;
+        }
+
+        const currentArr = letterMap[digits[currentIdx]];
+        for (let i = 0; i < currentArr.length; i++) {
+            path.push(currentArr[i]);
+            backtrack(path, currentIdx + 1);
+            path.pop();
+        }
+
+    }
+
+    backtrack(path, 0);
+
+    return result;
+};
+// @lc code=end
+
