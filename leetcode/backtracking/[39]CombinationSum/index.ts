5fcb2838afc90272aff531c5c45945830f58a27b
qiujiajun <545509146@qq.com>
2024-06-25 20:26:32 +0800

    feat: solution of 39

--- leetcode/backtracking/[39]CombinationSum/index.ts
+++ leetcode/backtracking/[39]CombinationSum/index.ts
@@ -6,7 +6,33 @@
 
 // @lc code=start
 function combinationSum(candidates: number[], target: number): number[][] {
-    
+    const result: number[][] = [];
+
+    const backtrack = (sum: number, path: number[], startIndex: number): void => {
+        if (sum > target) {
+            return;
+        }
+
+        if (sum === target) {
+            result.push([...path]);
+
+            return;
+        }
+
+        for (let i = startIndex, len = candidates.length; i < len; i++) {
+            const candidate: number = candidates[i];
+
+            sum += candidate;
+            path.push(candidate);
+            backtrack(sum, path, i);
+            path.pop();
+            sum -= candidate;
+        }
+    }
+
+    backtrack(0, [], 0);
+
+    return result;
 };
 // @lc code=end
 
