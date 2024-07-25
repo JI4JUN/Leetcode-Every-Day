fc4d71e286d12405d934e64d5cbac59cce9a6ffd
qiujiajun <545509146@qq.com>
2024-07-25 18:07:33 +0800

    test: Write a test for Call function

---
+++ Theory/DataStructure/utils/AbstractOperations/OperationsOnObjects/__tests__/Call.test.js
@@ -0,0 +1,47 @@
+import { Call } from '../index';
+
+describe('Cal function', () => {
+    function testFunction() {
+        return 'testValue';
+    }
+
+    test('Call callable function with argumentsList', () => {
+        const argumentsList = [1, 2, 3];
+
+        function sum(a, b, c) {
+            return a + b + c;
+        }
+
+        expect(Call(sum, undefined, argumentsList)).toBe(6);
+    });
+
+    test('Call callable function without argumentsList', () => {
+        expect(Call(testFunction, undefined)).toBe('testValue');
+    });
+
+    test('Throws TypeError for non-callable function', () => {
+        expect(() => {
+            Call({}, undefined);
+        }).toThrow(TypeError);
+
+        expect(() => {
+            Call(null, undefined);
+        }).toThrow(TypeError);
+
+        expect(() => {
+            Call(undefined, undefined);
+        }).toThrow(TypeError);
+
+        expect(() => {
+            Call('string', undefined);
+        }).toThrow(TypeError);
+    });
+
+    test('Uses empty argumentsList when not provided', () => {
+        function checkArgumentsList() {
+            return arguments.length;
+        }
+
+        expect(Call(checkArgumentsList, undefined)).toBe(0);
+    });
+});
