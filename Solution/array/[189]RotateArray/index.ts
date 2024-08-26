c800f2aacd1be1c21484fd5fbc5e7280372dd3c9
qiujiajun <545509146@qq.com>
2024-08-26 13:39:18 +0800

    feat: Solve the question of 189

---
+++ Solution/array/[189]RotateArray/index.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode id=189 lang=typescript
+ *
+ * [189] Rotate Array
+ */
+
+// @lc code=start
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+// ======================== Approach 1 ======================== //
+function rotate1(nums: number[], k: number): void {
+    while (k--) {
+        nums.unshift(nums.pop()!);
+    }
+}
+
+// ======================== Approach 2 ======================== //
+function rotate2(nums: number[], k: number): void {
+    const len: number = nums.length;
+    const reverseSegement = (start: number, end: number): void => {
+        while (start < end) {
+            [nums[start++], nums[end--]] = [nums[end], nums[start]];
+        }
+    };
+
+    k %= len;
+
+    reverseSegement(0, len - k - 1);
+    reverseSegement(len - k, len - 1);
+    nums.reverse();
+}
+
+// ======================== Approach 3 ======================== //
+function rotate(nums: number[], k: number): void {
+    const len: number = nums.length;
+    const tempArr: number[] = new Array(len).fill(0);
+
+    k %= len;
+
+    for (let i = 0; i < len; ++i) {
+        tempArr[(i + k) % len] = nums[i];
+    }
+
+    for (let i = 0; i < len; ++i) {
+        nums[i] = tempArr[i];
+    }
+}
+// @lc code=end
