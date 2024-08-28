a6cee42cc09f7e6029357af89f54149f1e467cfc
qiujiajun <545509146@qq.com>
2024-08-28 19:52:01 +0800

    feat: Implement optimized approach of question 274

--- Solution/hash-table/[274]HIndex/index.ts
+++ Solution/hash-table/[274]HIndex/index.ts
@@ -5,7 +5,8 @@
  */
 
 // @lc code=start
-function hIndex(citations: number[]): number {
+// ======================== Approach 1 ======================== //
+function hIndex1(citations: number[]): number {
     const len: number = citations.length;
 
     citations.sort((a, b) => a - b);
@@ -13,6 +14,25 @@ function hIndex(citations: number[]): number {
     for (let i = 0; i < len; ++i) {
         if (citations[i] >= len - i) {
             return len - i;
+        }
+    }
+
+    return 0;
+}
+
+// ======================== Approach 2 ======================== //
+function hIndex(citations: number[]): number {
+    const len: number = citations.length;
+    const frequency: number[] = citations.reduce(
+        (acc, cur) => (cur < len ? (++acc[cur], acc) : (++acc[len], acc)),
+        new Array(len + 1).fill(0)
+    );
+
+    for (let i = len, count = 0; i >= 0; --i) {
+        count += frequency[i];
+
+        if (count >= i) {
+            return i;
         }
     }
 
