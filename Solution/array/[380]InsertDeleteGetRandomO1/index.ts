a9434ca84e6d5fb135ae92c09ec1b6961585a957
qiujiajun <545509146@qq.com>
2024-08-30 11:06:15 +0800

    feat: Solve the question of 380

---
+++ Solution/array/[380]InsertDeleteGetRandomO1/index.ts
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=380 lang=typescript
+ *
+ * [380] Insert Delete GetRandom O(1)
+ */
+
+// @lc code=start
+class RandomizedSet {
+    private list: number[];
+    private map: Map<number, number>;
+
+    constructor() {
+        this.list = [];
+        this.map = new Map();
+    }
+
+    insert(val: number): boolean {
+        return this.map.has(val)
+            ? false
+            : (this.list.push(val),
+              this.map.set(val, this.list.length - 1),
+              true);
+    }
+
+    remove(val: number): boolean {
+        if (!this.map.has(val)) {
+            return false;
+        }
+
+        const index: number = this.map.get(val)!;
+
+        this.list[index] = this.list[this.list.length - 1];
+        this.map.set(this.list[index], index);
+        this.map.delete(val);
+        this.list.pop();
+
+        return true;
+    }
+
+    getRandom(): number {
+        return this.list[Math.floor(Math.random() * this.list.length)];
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * var obj = new RandomizedSet()
+ * var param_1 = obj.insert(val)
+ * var param_2 = obj.remove(val)
+ * var param_3 = obj.getRandom()
+ */
+// @lc code=end
