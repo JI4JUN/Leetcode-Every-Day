e0bd074200a1f6bbea5540b128d74844c13f35b7
qiujiajun <545509146@qq.com>
2024-09-02 11:32:47 +0800

    feat: Implement a KMP approach for question 28

--- Solution/two-pointers/[28]FindTheIndexOfTheFirstOccurrenceInAString/index.ts
+++ Solution/two-pointers/[28]FindTheIndexOfTheFirstOccurrenceInAString/index.ts
@@ -5,6 +5,7 @@
  */
 
 // @lc code=start
+// ======================== Approach 1 ======================== //
 function strStr1(haystack: string, needle: string): number {
     const needleLen: number = needle.length;
 
@@ -24,7 +25,51 @@ function strStr1(haystack: string, needle: string): number {
     return -1;
 }
 
-function strStr(haystack: string, needle: string): number {
+// ======================== Approach 2 ======================== //
+function strStr2(haystack: string, needle: string): number {
     return haystack.indexOf(needle);
+}
+
+// ======================== Approach 3 ======================== //
+function strStr(haystack: string, needle: string): number {
+    const createNext = (str: string): number[] => {
+        const strLen: number = str.length;
+        let next: number[] = new Array(strLen).fill(-1);
+        let j: number = -1;
+
+        for (let i = 1; i < strLen; ++i) {
+            while (j >= 0 && str[i] !== str[j + 1]) {
+                j = next[j];
+            }
+
+            if (str[i] === str[j + 1]) {
+                ++j;
+            }
+
+            next[i] = j;
+        }
+
+        return next;
+    };
+
+    const next: number[] = createNext(needle);
+    const needleLen: number = needle.length;
+    let j: number = -1;
+
+    for (let i = 0; i < haystack.length; ++i) {
+        while (j >= 0 && haystack[i] !== needle[j + 1]) {
+            j = next[j];
+        }
+
+        if (haystack[i] === needle[j + 1]) {
+            ++j;
+        }
+
+        if (j === needleLen - 1) {
+            return i - needleLen + 1;
+        }
+    }
+
+    return -1;
 }
 // @lc code=end
