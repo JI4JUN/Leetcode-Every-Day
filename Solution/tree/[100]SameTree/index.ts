030645fcf2767328a2633ad8210ee19c8729238a
qiujiajun <545509146@qq.com>
2024-09-16 23:03:35 +0800

    feat: Solve the question 100

---
+++ Solution/tree/[100]SameTree/index.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=100 lang=typescript
+ *
+ * [100] Same Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+    if (p === null && q === null) {
+        return true;
+    }
+
+    if (p === null || q === null) {
+        return false;
+    }
+
+    if (p.val === q.val) {
+        const { left: pLeft, right: pRight } = p;
+        const { left: qLeft, right: qRight } = q;
+
+        return isSameTree(pLeft, qLeft) && isSameTree(pRight, qRight);
+    }
+
+    return false;
+}
+// @lc code=end
