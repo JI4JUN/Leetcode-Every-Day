74794e3d97af3b3282e35d0c6775b5227b444034
qiujiajun <545509146@qq.com>
2024-10-06 15:52:56 +0800

    feat: Add another soultion of the question 200

--- Solution/depth-first-search/[200]NumberOfIslands/index.ts
+++ Solution/depth-first-search/[200]NumberOfIslands/index.ts
@@ -5,11 +5,12 @@
  */
 
 // @lc code=start
-function numIslands(grid: string[][]): number {
+// ======================== Approach 1 ======================== //
+function numIslands1(grid: string[][]): number {
     const m: number = grid.length;
     const n: number = grid[0].length;
     const visited: boolean[][] = Array.from({ length: m }, () =>
-        Array(n).fill(false)
+        new Array(n).fill(false)
     );
     const directions: number[] = [-1, 0, 1, 0, -1];
     const dfs = (r: number, c: number): void => {
@@ -35,6 +36,53 @@ function numIslands(grid: string[][]): number {
                 ++acc;
 
                 dfs(r, c);
+            }
+
+            return acc;
+        }, result);
+
+        return result;
+    }, 0);
+}
+
+// ======================== Approach 2 ======================== //
+function numIslands(grid: string[][]): number {
+    const m: number = grid.length;
+    const n: number = grid[0].length;
+    const visited: boolean[][] = Array.from({ length: m }, () =>
+        new Array(n).fill(false)
+    );
+    const directions: number[] = [-1, 0, 1, 0, -1];
+    const bfs = (r: number, c: number): void => {
+        const queue: [number, number][] = [[r, c]];
+        visited[r][c] = true;
+
+        while (queue.length > 0) {
+            const [currX, currY]: [number, number] = queue.shift()!;
+
+            for (let i = 0; i < directions.length; ++i) {
+                const x: number = currX + directions[i];
+                const y: number = currY + directions[i + 1];
+
+                if (x >= 0 && x < m && y >= 0 && y < n) {
+                    if (!visited[x][y] && grid[x][y] === '1') {
+                        queue.push([x, y]);
+
+                        visited[x][y] = true;
+                    }
+                }
+            }
+        }
+    };
+
+    return grid.reduce((result, rows, r) => {
+        result = rows.reduce((acc, el, c) => {
+            if (!visited[r][c] && el === '1') {
+                visited[r][c] = true;
+
+                ++acc;
+
+                bfs(r, c);
             }
 
             return acc;
