dac8f6e2b422d778ae1713c9466d409d7c2ec3be
qiujiajun <545509146@qq.com>
2024-10-08 16:24:04 +0800

    feat: Add another TS solution of question 133

--- Solution/graph/[133]CloneGraph/index.ts
+++ Solution/graph/[133]CloneGraph/index.ts
@@ -18,7 +18,38 @@
  * }
  *
  */
+// ======================== Approach 1 ======================== //
+function cloneGraph1(node: _Node | null): _Node | null {
+    if (node === null) {
+        return null;
+    }
+    if (node.neighbors.length === 0) {
+        return new _Node(node.val);
+    }
 
+    const nodeMap: Map<_Node, _Node> = new Map();
+    const dfs = (currNode: _Node): _Node => {
+        const clone: _Node = new _Node(currNode.val);
+
+        nodeMap.set(currNode, clone);
+
+        currNode.neighbors.forEach((node: _Node): void => {
+            if (nodeMap.has(node)) {
+                clone.neighbors.push(nodeMap.get(node)!);
+            } else {
+                const neighbor: _Node = dfs(node);
+
+                clone.neighbors.push(neighbor);
+            }
+        });
+
+        return clone;
+    };
+
+    return dfs(node);
+}
+
+// ======================== Approach 2 ======================== //
 function cloneGraph(node: _Node | null): _Node | null {
     if (node === null) {
         return null;
@@ -27,28 +58,23 @@ function cloneGraph(node: _Node | null): _Node | null {
         return new _Node(node.val);
     }
 
-    const nodeMap: Map<_Node, _Node> = new Map<_Node, _Node>();
-    const dfs = (currNode: _Node): _Node => {
-        const neighbors: _Node[] = [];
-        const clone: _Node = new _Node(currNode.val);
+    const nodeMap: Map<_Node, _Node> = new Map([[node, new _Node(node.val)]]);
+    const queue: _Node[] = [node];
 
-        nodeMap.set(currNode, clone);
+    while (queue.length > 0) {
+        const currNode: _Node = queue.shift()!;
+        const clone: _Node = nodeMap.get(currNode)!;
 
         currNode.neighbors.forEach((node: _Node): void => {
-            if (nodeMap.has(node)) {
-                neighbors.push(nodeMap.get(node)!);
-            } else {
-                const neighbor: _Node = dfs(node);
-
-                neighbors.push(neighbor);
+            if (!nodeMap.has(node)) {
+                nodeMap.set(node, new _Node(node.val));
+                queue.push(node);
             }
+
+            clone.neighbors.push(nodeMap.get(node)!);
         });
+    }
 
-        clone.neighbors = neighbors;
-
-        return clone;
-    };
-
-    return dfs(node);
+    return nodeMap.get(node)!;
 }
 // @lc code=end
