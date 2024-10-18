80fb8bbc0569bd1e32bb0e39e7548d0ad778e9bc
qiujiajun <545509146@qq.com>
2024-10-18 12:11:19 +0800

    feat: Solve the question 139

--- Solution/dynamic-programming/[139]WordBreak/index.ts
+++ Solution/dynamic-programming/[139]WordBreak/index.ts
@@ -6,7 +6,27 @@
 
 // @lc code=start
 function wordBreak(s: string, wordDict: string[]): boolean {
-    
-};
-// @lc code=end
+    const wordsSet: Set<string> = new Set(wordDict);
+    const dp: boolean[] = new Array(s.length + 1).fill(false);
 
+    dp[0] = true;
+
+    for (let i = 1; i <= s.length; ++i) {
+        for (const word of wordsSet) {
+            const startIndex: number = i - word.length;
+
+            if (
+                startIndex >= 0 &&
+                dp[startIndex] &&
+                s.substring(startIndex, i) === word
+            ) {
+                dp[i] = true;
+
+                break;
+            }
+        }
+    }
+
+    return dp[s.length];
+}
+// @lc code=end
