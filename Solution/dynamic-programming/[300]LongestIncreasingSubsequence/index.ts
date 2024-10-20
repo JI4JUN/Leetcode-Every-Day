f8693309430560219582678a59f10c83010053f9
qiujiajun <545509146@qq.com>
2024-10-20 14:58:32 +0800

    feat: Add priority BS method to the question 300

--- Solution/dynamic-programming/[300]LongestIncreasingSubsequence/index.ts
+++ Solution/dynamic-programming/[300]LongestIncreasingSubsequence/index.ts
@@ -5,7 +5,8 @@
  */
 
 // @lc code=start
-function lengthOfLIS(nums: number[]): number {
+// ======================== Approach 1 ======================== //
+function lengthOfLIS1(nums: number[]): number {
     const numsLen: number = nums.length;
     const dp: number[] = new Array(numsLen).fill(1);
 
@@ -18,5 +19,38 @@ function lengthOfLIS(nums: number[]): number {
     }
 
     return Math.max(...dp);
+}
+
+// ======================== Approach 2 ======================== //
+function lengthOfLIS(nums: number[]): number {
+    const result: number[] = [];
+    const BS = (target: number): number => {
+        let left: number = 0;
+        let right: number = result.length - 1;
+
+        while (left <= right) {
+            const mid: number = left + ((right - left) >> 1);
+
+            if (result[mid] < target) {
+                left = mid + 1;
+            } else if (result[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return left;
+    };
+
+    nums.forEach((num) => {
+        if (result.length === 0 || result.at(-1)! < num) {
+            result.push(num);
+        } else {
+            result[BS(num)] = num;
+        }
+    });
+
+    return result.length;
 }
 // @lc code=end
